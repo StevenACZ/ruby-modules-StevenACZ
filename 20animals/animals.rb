9d676e1cf29f752309bf17285330fef91f221e71
stevenacz <scoailazaa@gmail.com>
2020-11-11 17:09:19 -0500

    Problema 20

--- 20animals/animals.rb
+++ 20animals/animals.rb
@@ -1,15 +1,43 @@
 module Runner
-  # Your code here
+  attr_writer :specie
+  VELOCITIES = { "caballus" => 40, "tigris" => 35 }
+
+  def run(time)
+    time = case specie
+    when "caballus" then time * VELOCITIES["caballus"]
+    when "tigris" then time * VELOCITIES["tigris"]
+    end
+    puts "I have run #{time} kilometers"
+  end
 end
 
 class Animal
-  # Your code here
+  attr_reader :specie, :name
+
+  def initialize(new_animal = {})
+    @specie = new_animal[:specie].nil? ? "Unknown" : new_animal[:specie]
+    @name = new_animal[:name].nil? ? "Unknown" : new_animal[:name]
+  end
+
+  def to_s
+    "My name is #{@name} and I'm a #{@specie}"
+  end
 end
 
-class Horse
-  # Your code here and maybe update the class definition ;)
+class Horse < Animal
+  include Runner
+
+  def initialize(name = "Unknown")
+    @specie = "caballus"
+    @name = name
+  end
 end
 
-class Tiger
-  # Your code here and maybe update the class definition ;)
+class Tiger < Animal
+  include Runner
+
+  def initialize(name = "Unknown")
+    @specie = "tigris"
+    @name = name
+  end
 end
